82d418377757cd5935893f665ee5357881727977
CsAdi <oradam86@gmail.com>
2023-10-05 13:26:35 +0200

    01:11:00 : TDD Clean Architecture

---
+++ android/app/src/main/kotlin/com/tdd_clean_architecture/tdd_clean_architecture/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tdd_clean_architecture.tdd_clean_architecture
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/authentication/domain/repositories/authentication_repository.dart
@@ -0,0 +1,3 @@
+/// Authentication Repository: It means what functionality /-ies this feature (authentication) carries,
+/// but it does not actually implement them.
+/// Authentication is a feature (So basically we could put it in a folder, named features)
