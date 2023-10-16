de0d53f6609c183a05e21de0231aba497cf1c66d
CsAdi <oradam86@gmail.com>
2023-10-16 11:46:38 +0200

    06:45:00 : Cubit | Test Cubit | Dependency injection | Next: UI

--- lib/main.dart
+++ lib/main.dart
@@ -17,11 +17,7 @@ class MainApp extends StatelessWidget {
       theme: ThemeData(
         // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
-      ),
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World'),
-        ),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
     );
   }
